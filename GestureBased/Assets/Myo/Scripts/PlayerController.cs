05f5c0eacce7734ae75872ad01587bd702cc6e26
Riona Greally <rionagreally@gmail.com>
2018-04-05 16:37:08 +0100

    Road moving and repeating

---
+++ GestureBased/Assets/Myo/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    public float force;
+
+    private Rigidbody2D myRigidBody;
+
+
+	// Use this for initialization
+	void Start () {
+        myRigidBody = GetComponent<Rigidbody2D>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        myRigidBody.velocity = new Vector2(speed, myRigidBody.velocity.y);
+
+        /*if(Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        {
+            myRigidBody.velocity = new Vector2(myRigidBody.velocity.x, force);
+        }
+        */
+	}
+}
---
+++ GestureBased/Assets/RepeatingBackground.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatingBackground : MonoBehaviour {
+
+    private BoxCollider2D bgCollder;
+    private float bgVerticalLength;
+
+	// Use this for initialization
+	void Start () {
+
+        bgCollder = GetComponent<BoxCollider2D>();
+        bgVerticalLength = bgCollder.size.y;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(transform.position.y < -bgVerticalLength)
+        {
+            RepositionBackground();
+        }
+	}
+
+    private void RepositionBackground()
+    {
+        Vector2 bgOffset = new Vector2(0, bgVerticalLength * 2);
+        transform.position = (Vector2)transform.position + bgOffset;
+    }
+}
