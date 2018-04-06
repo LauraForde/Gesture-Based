0bf5d48a74b87e591f605cb9f46d4be66e470c20
Riona Greally <rionagreally@gmail.com>
2018-04-06 14:36:45 +0100

    destroy point for memory addeed

---
+++ GestureBased/Assets/Scripts/Destroyer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroyer : MonoBehaviour {
+
+    public GameObject destroyPoint;
+
+	// Use this for initialization
+	void Start () {
+
+        destroyPoint = GameObject.Find("DestroyPoint");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (transform.position.y < destroyPoint.transform.position.y)
+        {
+
+            //Destroy (gameObject);
+            gameObject.SetActive(false);
+        }
+
+    }
+}
