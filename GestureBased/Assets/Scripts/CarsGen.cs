906ed3ecd5365282c84ede30645c8e479894b4ac
Riona Greally <rionagreally@gmail.com>
2018-04-10 14:12:14 +0100

    attemting cars generating

--- GestureBased/Assets/Scripts/CarsGen.cs
+++ GestureBased/Assets/Scripts/CarsGen.cs
@@ -4,28 +4,37 @@ using UnityEngine;
 
 public class CarsGen : MonoBehaviour {
 
+   // public ObjectPooler objPool;
+   // public float distance;
+    public GameObject car;
+    public Transform genPoint;
+    private float width;
     public ObjectPooler objPool;
-    public float distance;
-
-    /*public void EvilCar(Vector3 ){
-		
-	}*/
-    float carMax = 0.6f; 
-    float carMin = -0.35f;
 
   
+
+
     void Start()
     {
-        GameObject[] cars = GameObject.FindGameObjectsWithTag("Player"); 
-
         
-        foreach (GameObject car in cars)
-        { 
-            Vector3 pos = car.transform.position; 
-            pos.y = Random.Range(carMin, carMax); 
-            car.transform.position = pos; 
+            width = car.GetComponent<BoxCollider2D>().size.y;
+
+    }
+
+    private void Update()
+    {
+        if (transform.position.y < genPoint.position.y)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y + width, transform.position.z);
+            //Instantiate (platform, transform.position, transform.rotation);
+
+            GameObject newCar = objPool.GetPooledObject();
+
+            newCar.transform.position = transform.position;
+            newCar.transform.rotation = transform.rotation;
+            newCar.SetActive(true);
         }
     }
-  
+
 
 }
