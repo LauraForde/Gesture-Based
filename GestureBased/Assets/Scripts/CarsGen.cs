46ff53a7901108495010d3abccc908b5b85844c3
Riona Greally <rionagreally@gmail.com>
2018-04-10 01:11:43 +0100

    cars generting

--- GestureBased/Assets/Scripts/CarsGen.cs
+++ GestureBased/Assets/Scripts/CarsGen.cs
@@ -4,10 +4,28 @@ using UnityEngine;
 
 public class CarsGen : MonoBehaviour {
 
-	public ObjectPooler objPool;
-	public float distance;
+    public ObjectPooler objPool;
+    public float distance;
 
-	/*public void EvilCar(Vector3 ){
+    /*public void EvilCar(Vector3 ){
 		
 	}*/
+    float carMax = 0.6f; // Maximum height a cloud pair can be set to 
+    float carMin = -0.35f; // Minimum height
+
+  
+    void Start()
+    {
+        GameObject[] cars = GameObject.FindGameObjectsWithTag("Player"); 
+
+        
+        foreach (GameObject car in cars)
+        { 
+            Vector3 pos = car.transform.position; //current position
+            pos.y = Random.Range(carMin, carMax); // Set the new position to a random range between min / max set above
+            car.transform.position = pos; // move the first tile to the new position
+        }
+    }
+  
+
 }
