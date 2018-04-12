bd432fcfca71c0ae833215ac3700f55b02ead2b4
LauraForde <G00315765@gmit.ie>
2018-04-12 20:48:54 +0100

    Collisions working

---
+++ GestureBased/Assets/Scripts/ColDetection.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ColDetection : MonoBehaviour {
+
+	private Rigidbody2D myRigidBody;
+
+	void OnTriggerEnter2D(Collider2D other){
+		if(other.gameObject.tag == "Player"){
+			SceneManager.LoadScene("GameOver", LoadSceneMode.Single);
+		}
+	}
+}
